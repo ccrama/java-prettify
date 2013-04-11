6105262fa3e3cbe4f1ed8e6f3d4b34b1df835b98
marc.strapetz <marc.strapetz@gmail.com>
2013-04-11 10:50:25 +0000

    - update to r244 of google-code-prettify (better basic and pascal support courtesy Peter Kopfler in issue 137)

--- test/prettify/PrettifyTest.java
+++ test/prettify/PrettifyTest.java
@@ -170,6 +170,8 @@ public class PrettifyTest {
     test("r", "r_lang", false);
     test("erlang", "erlang", false);
     test("mumps", "mumps", false);
+    test("pascal", "pascal_lang", false);
+    test("basic", "basic_lang", false);
   }
 
   /**
